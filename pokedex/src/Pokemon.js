53373b96a242798103fe3b75d1d14d25d2b9ff94
rover33 <remy.j.pearlstone@gmail.com>
2020-03-24 15:58:39 -0600

    updating state

--- pokedex/src/Pokemon.js
+++ pokedex/src/Pokemon.js
@@ -1,22 +1,15 @@
 import React, {useState, useEffect} from 'react';
-
-
 let Pokemon = () => {
   let [pokemon, setPokemon] = useState([]);
-  // let [img, setImg] = useState(null);
-  // let [num, setNumb] = useState([])
-
+  let [searchString, setSearchString] = useState('');
+  let [elementObj, setElementObj] = useState({fire: false, water: false, poison:false, grass:false})
 
   useEffect(() => {
     let isCurrent = true;
     fetch(`https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json`)
-
       .then(res => res.json())
       .then(res => {
-        if (isCurrent) 
-        setPokemon(res.pokemon)
-        // setNumb(res.number)
-        // setImg(res.img)
+        if (isCurrent) setPokemon(res.pokemon)
       })
       .catch(() => {
         console.log("stop it");
@@ -26,10 +19,33 @@ let Pokemon = () => {
       };
     }, [pokemon]);
 
+
+  let checkboxClick = e => {
+    setElementObj(elementObj);
+    console.log(elementObj)
+    e.stopPropagation();
+  }
+
+  let onChange = e => setSearchString(e.target.value);
+
   let renderPokedex = () => {
-    return (
-    <ul>
-      {pokemon.map(el => (
+    let tempArr = []
+    if (searchString.length <= 0) {
+      tempArr = pokemon
+    } else {
+      for (let i = 0; i < pokemon.length; i++) {
+        if (pokemon[i].name.toLowerCase().includes(searchString)) {
+          tempArr.push(pokemon[i])
+        }
+        if (checkboxClick && pokemon[i].type.includes(elementObj)) {
+          tempArr.push(pokemon[i])
+        }
+    }
+  }
+
+
+    return <ul>
+      {tempArr.map(el => (
       <li key={el.id}>
         Name: {el.name}
         <br />
@@ -43,32 +59,45 @@ let Pokemon = () => {
         </li>
       ))}
     </ul>
-  )}
+  }
 
   return (
     <div>
        <input
         type="search"
         placeholder="pokemon"
-        onChange={e => {
-          setPokemon(e.target.value);
-      }}
+        onChange={onChange}
     />
+    <br />
+    <br />
+    <div>
+      <label>Pokemon Element Type</label>
+      <br/>
+      <input type="checkbox" id="grass" name="grass" onClick={checkboxClick}/>
+      <label htmlFor="grass">Grass</label>
+      <input type="checkbox" id="poison" name="poison" onClick={checkboxClick}/>
+      <label htmlFor="posion">Poison</label>
+      <input type="checkbox" id="water" name="water" onClick={checkboxClick}/>
+      <label htmlFor="water">Water</label>
+      <input type="checkbox" id="fire" name="fire" onClick={checkboxClick}/>
+      <label htmlFor="fire">Fire</label>
+    </div>
+    <br />
+    <br />
+    {/* <div>
+      <label>Pokemon Weakness</label>
+      <br />
+      <input type="checkbox" id="weakgrass" name="weakgrass" onClick={checkboxClick()}/>
+      <label for="weakgrass">Grass</label>
+      <input type="checkbox" id="weakpoison" name="weakpoison"/>
+      <label for="weakposion">Poison</label>
+      <input type="checkbox" id="weakwater" name="weakwater"/>
+      <label for="weakwater">Water</label>
+      <input type="checkbox" id="weakfire" name="weakfire"/>
+      <label for="weakfire">Fire</label>
+    </div> */}
     {renderPokedex()}
-    {/* Hello, {pokemon}!
-      {img && <img src={img} />}
-      {num && <div>{num}</div>} */}
     </div>
   );
 };
-
 export default Pokemon
-
-
-
-
-
-
-
-
-
